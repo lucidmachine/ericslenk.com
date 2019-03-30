323b079c65a0f76219ca593c28182ca8aa7303ed
lucidmachine <lucid.machine@gmail.com>
2019-03-30 14:24:30 -0400

    Replace LinkedIn link with RSS feed link.

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,6 +2,19 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+# Metadata
+AUTHOR = 'Eric Slenk'
+DEFAULT_LANG = 'en'
+GITHUB_URL = 'https://github.com/lucidmachine'
+SITELOGO = "https://avatars1.githubusercontent.com/u/213561"
+SITETITLE = "Lucid Machinery"
+SITESUBTITLE = "A Blog by Eric Slenk"
+SITENAME = SITETITLE
+SITEDESCRIPTION = SITESUBTITLE
+TIMEZONE = 'America/Detroit'
+TWITTER_USERNAME = 'lucidmachinery'
+
+
 # Dates
 DEFAULT_DATE_FORMAT = '%Y-%m-%d'
 
@@ -14,24 +27,6 @@ OBFUSCATED_EMAIL = ''.join(['&#{0:s};'.format(str(ord(char))) for char in EMAIL_
 GOOGLE_ANALYTICS = "UA-101906012-1"
 GOOGLE_TAG_MANAGER = "GTM-NW29DZR"
 
-# Links
-SOCIAL = (('envelope-open', OBFUSCATED_EMAIL),
-        ('twitter', 'https://twitter.com/lucidmachinery'),
-        ('github', 'https://github.com/lucidmachine'),
-        ('linkedin', 'https://www.linkedin.com/in/eric-slenk-167aa67a/'),)
-
-
-# Metadata
-AUTHOR = 'Eric Slenk'
-DEFAULT_LANG = 'en'
-GITHUB_URL = 'https://github.com/lucidmachine'
-SITELOGO = "https://avatars1.githubusercontent.com/u/213561"
-SITETITLE = "Lucid Machinery"
-SITESUBTITLE = "A Blog by Eric Slenk"
-SITENAME = SITETITLE
-SITEDESCRIPTION = SITESUBTITLE
-TIMEZONE = 'America/Detroit'
-TWITTER_USERNAME = 'lucidmachinery'
 
 # Pagination
 DEFAULT_PAGINATION = 10
@@ -77,3 +72,10 @@ FEED_MAX_ITEMS = None
 TAG_FEED_ATOM = 'feeds/{slug}.atom.xml'
 TAG_FEED_RSS = 'feeds/{slug}.rss.xml'
 
+# Links
+SOCIAL = (
+    ('envelope-open', OBFUSCATED_EMAIL),
+    ('twitter', 'https://twitter.com/lucidmachinery'),
+    ('github', 'https://github.com/lucidmachine'),
+    ('rss', FEED_ALL_RSS),
+)
